75ba931b56716a6c0d112317ea7d2d963be939b5
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-10-07 13:51:04 -0500

    added modules

---
+++ server/modules/quotes.js
@@ -0,0 +1,7 @@
+let quotesData = [
+    { quote: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
+    { quote: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
+    { quote: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
+];
+
+module.exports = quotesData;
--- server/server.js
+++ server/server.js
@@ -1,25 +1,33 @@
+// Require express - gives us a function
 const express = require('express');
 
+// Create an instance of express by calling the function 
+// returned above - gives us an object
 const app = express();
 const port = 5000;
 
+const quotesData = require ('./modules/quotes.js');
+
+// express static file serving - public is the folder name
 app.use(express.static('server/public'));
 
-let quotesData = [
-    { quote: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
-    { quote: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
-    { quote: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
-];
 
 let index = 0;
 
 app.get('/quotes', (req, res) => {
     console.log('hi from get request');
-    
-    res.sendStatus(200);
+    res.send(quotesData);
 })
 
+app.get('/randomQuote', (req, res) => {
+    let randomNumber = getRandomInt(quotesData.length);
+    res.send(quotesData[randomNumber]);
+});
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
 app.listen(port, () => {
-    console.log('up and running on port', port);
-    
-})
+    console.log("Up and running on port: ", port);
+});
